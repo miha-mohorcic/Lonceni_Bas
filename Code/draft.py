0fcf2eae4068ff545744391da6ef5dee879e86d2
Miha Mohorčič <mohimm@gmail.com>
2015-07-08 10:24:16 +0000

    Adding first files

---
+++ Code/draft.py
@@ -0,0 +1,58 @@
+import sys
+import time
+
+import Adafruit_MPR121.MPR121 as MPR121
+
+print 'Adafruit MPR121 Capacitive Touch Sensor Test'
+
+# Create MPR121 instance.
+cap = MPR121.MPR121()
+
+# Initialize communication with MPR121 using default I2C bus of device, and 
+# default I2C address (0x5A).  On BeagleBone Black will default to I2C bus 0.
+if not cap.begin():
+    print 'Error initializing MPR121.  Check your wiring!'
+    sys.exit(1)
+
+# Main loop to print a message every time a pin is touched.
+print 'Press Ctrl-C to quit.'
+last_touched = cap.touched()
+
+timing = [0,0,0,0,0,0,0,0,0,0,0,0]
+cap.set_thresholds(2,2)
+
+
+while True:
+    current_touched = cap.touched()
+    # Check each pin's last and current state to see if it was pressed or released.
+    for i in range(5):
+        # Each pin is represented by a bit in the touched value.  A value of 1
+        # means the pin is being touched, and 0 means it is not being touched.
+        pin_bit = 1 << i
+        # First check if transitioned from not touched to touched.
+        if current_touched & pin_bit and not last_touched & pin_bit:
+            timing[i] = time.time()
+            #print timing
+            #print '{0} touched!'.format(i)
+
+        # Next check if transitioned from touched to not touched.
+        if not current_touched & pin_bit and last_touched & pin_bit:
+            print '{0}'.format(i), 'touched for ', timing[i]-time.time()
+
+    # Update last state and wait a short period before repeating.
+    last_touched = current_touched
+    time.sleep(0.02)
+
+    # Alternatively, if you only care about checking one or a few pins you can 
+    # call the is_touched method with a pin number to directly check that pin.
+    # This will be a little slower than the above code for checking a lot of pins.
+    #if cap.is_touched(0):
+    #    print 'Pin 0 is being touched!'
+    
+    # If you're curious or want to see debug info for each pin, uncomment the
+    # following lines:
+    #print '\t\t\t\t\t\t\t\t\t\t\t\t\t 0x{0:0X}'.format(cap.touched())
+    #filtered = [cap.filtered_data(i) for i in range(12)]
+    #print 'Filt:', '\t'.join(map(str, filtered))
+    #base = [cap.baseline_data(i) for i in range(12)]
+    #print 'Base:', '\t'.join(map(str, base))
