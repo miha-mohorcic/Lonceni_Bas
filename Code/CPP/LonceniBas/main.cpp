801fc5410e1b879b5e202d0b16f705bb6f9b93be
Miha Mohorčič <mohimm@gmail.com>
2015-07-13 21:41:42 +0000

    adding third project (need to clean later first two)

--- Code/CPP/LonceniBas/main.cpp
+++ Code/CPP/LonceniBas/main.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "MPR121.h"
+#include "I2Cdev.h"
+
+//#include "wiringPi.h"
+//#include "wiringPiI2C.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Start testing" << endl;
-    //MPR121* senzor = new MPR121();
-    MPR121 senzor = MPR121(MPR121_DEFAULT_ADDRESS);
-    cout << "1" << endl;
 
+    MPR121 senzor = MPR121(0x5a);
+
+    cout << "got class" <<endl;
     if (senzor.testConnection()){
         cout << "OK" << endl;
     }
+    else {
+        return 1;
+    }
 
     senzor.initialize();
-    cout << "2" << endl;
+
     bool touched;
-    cout << "3" << endl;
     while(true)
     {
-        cout << "get data" << endl;
         touched = senzor.getTouchStatus(0);
-        cout << "print data" << endl;
         cout << touched << endl;
     }
 
---
+++ Code/FricDrum/FrictionDrum/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "MPR/MPR121.h"
+#include "MPR/I2Cdev.h"
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello MPR121!" << endl;
+
+    MPR121 senzor = MPR121();
+    cout << "class" << endl;
+
+    if (senzor.testConnection()){
+        cout << "MPR121 OK" << endl;
+        cout << flush;
+    }
+    else {
+        cout << "MPR121 not OK" << endl;
+        cout << flush;
+        return 1;
+    }
+    cout << "init" << endl;
+
+    senzor.initialize();
+    cout << "loop" << endl;
+
+    bool touched;
+    while(true)
+    {
+        touched = senzor.getTouchStatus(0);
+        cout << touched << endl;
+    }
+
+    return 0;
+}
